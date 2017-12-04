82a21b932969f660035b7b1d3b8d5dfc774a4290
Bill Farner <wfarner@apache.org>
2017-12-04 12:08:22 -0800

    asdf

--- src/main/resources/org/apache/aurora/scheduler/storage/sql/schema.sql
+++ src/main/resources/org/apache/aurora/scheduler/storage/sql/schema.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS records(
+  -- 767 is the default max key length in MySQL
+  -- https://dev.mysql.com/doc/refman/5.7/en/create-index.html
   id VARCHAR(767) PRIMARY KEY,
   parent VARCHAR(767) DEFAULT NULL,
   value LONGBLOB,
